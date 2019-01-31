f709332fe087abe98ba56dadfa151403aa6900e8
zeonie <aviris.ordlien@gmail.com>
2019-01-31 11:36:11 +0300

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,25 @@
+function activeScroll(){
+    var scrollTop = $(document).scrollTop();
+    $("#nav a.scroll").each(function(){
+        sectionName = $(this).attr("href");
+        sectionPosition = $(sectionName).position().top - 80;
+        sectionEnd = sectionPosition + $(sectionName).outerHeight() + 80;
+        if ( sectionPosition <= scrollTop && sectionEnd > scrollTop){
+            $("#nav a.scroll").removeClass('active');
+            $(this).addClass('active');
+        } else {
+            $(this).removeClass('active');
+        }
+    });
+};
 $(document).ready(function(){
-    var mapHeight = $(".m-i-w").outerHeight();
-    $("#map").css('height',mapHeight);
+    $("#nav a.scroll, .adv-nav a.scroll").on('click', function(e){
+        e.preventDefault();
+        target = $(this).attr("href");
+        targetPosition = $(target).offset().top - 80;
+        $("html, body").animate({
+            scrollTop: targetPosition
+        }, 1000);
+    })
+    $(document).on("scroll", activeScroll);
 });
