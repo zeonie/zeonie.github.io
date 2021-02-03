27f31a0f8a26b3f8ffad8cc32103c4ed7f180392
zeonie <aviris.ordlien@gmail.com>
2021-02-03 23:39:01 +0300

    BELINTEREXPO-1 added gulp

---
+++ BelInterExpo/gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require('gulp');
+var less = require('gulp-less');
+var path = require('path');
+
+gulp.task('less', function() {
+    return gulp.src('dist/less/**/*.less')
+        .pipe(less({
+            paths: [ path.join(__dirname, 'less', 'includes') ]
+        }))
+        .pipe(gulp.dest('css'));
+});
+gulp.task('watch', function() {
+    gulp.watch('dist/less/**/*.less', gulp.parallel('less'));
+});
