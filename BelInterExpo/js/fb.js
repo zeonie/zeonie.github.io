0ddc5e2325820fe0461b29e268ce780db3ab838e
zeonie <s.voinich@belvg.com>
2021-02-13 15:10:30 +0300

    BELINEREXPO FB script

--- BelInterExpo/js/fb.js
+++ BelInterExpo/js/fb.js
@@ -9,12 +9,16 @@ window.fbAsyncInit = function() {
         '/belinterexpo.by/posts',
         'GET',
         {
-            access_token: 'EAAWxzUVWDqcBANEQZBZAjcNjyZBrcm2uh9HnMH9aWjr4cYrcr1C7xKXI9AbGd02xAHYhYcap6NxLtVZBmqk2W9ZA6m0QcHnVUZBW5kmpZBH9w9cjx8ZCmHU3dVIfm3evZCm3Ujz4ghYSS67dj8wrHP4Wg0nMYMg4ZBDOfZCMQGiDaZAvWSRlUohSJHfa9yqElZBZCwKZAbrr4ZAINlPtZCgZDZD',
-            fields:" full_picture,message,permalink_url,created_time",
+            access_token: 'EAAWxzUVWDqcBAAHZAm0UcZBM1bBZAkFEpCOi8ERkP8hkyuLCz4HxoISL1XGHKQ3c9KtYXIdhd0D00eEQ6nK0eahxMLrkEw3Oh5rLM7byuezrYZAtQZAmRKRPZCyEqEoZCmjEvrSmKJZAPY4YNYsdUcNOgICnMFwpy60pxyTY4MhCygi4RypObEaW',
+            fields:" full_picture,message,permalink_url",
             limit: "6"
         },
         function (response) {
-            console.log(response);
+            let $ = jQuery;
+            let need_data = response.data;
+            for ( let i = 0; i < need_data.length; i++ ) {
+                $('facebook_list').append('<div class="event-wrap"><div class="event"><div class="image-wrap"><img src="' + response.data[i].full_picture + '"></div><div class="event-text"><p>' + response.data[i].message + '</p></div><a href="' + response.data[i].permalink_url + '" target="_blank" class="arrow-right">Read more</a></div></div>');
+            }
         }
     );
 }
